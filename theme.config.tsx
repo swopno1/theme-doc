48fa046a5a4d2de54aded6be822fcba9234400e7
Md Amir Hossain <amirhossain.limon@gmail.com>
2022-12-12 20:13:47 +0600

    Updated code

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from 'react';
+import { DocsThemeConfig } from 'nextra-theme-docs';
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>AmirNFT Theme Doc</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/swopno1/theme-doc',
   },
   chat: {
     link: 'https://discord.com',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/swopno1/theme-doc',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'AmirNFT Theme Doc',
   },
-}
+};
 
-export default config
+export default config;
