71602d98568445c03d7bdca0d204bc9e7d9496d4
Md Amir Hossain <amirhossain.limon@gmail.com>
2022-12-23 11:39:58 +0600

    Added i18n localization with English and Bangla language.

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,3 +1,10 @@
+import { useConfig } from 'nextra-theme-docs';
+
+/* eslint sort-keys: error */
+/**
+ * @type {import('nextra-theme-docs').DocsThemeConfig}
+ */
+
 export default {
   logo: <span>AmirNFT Theme Doc</span>,
   project: {
@@ -6,8 +13,17 @@ export default {
   chat: {
     link: 'https://discord.com',
   },
-  docsRepositoryBase: 'https://github.com/swopno1/theme-doc',
+  docsRepositoryBase: 'https://github.com/swopno1/theme-doc/blob/main',
+  editLink: {
+    text: 'Edit this page on GitHub',
+  },
+  faviconGlyph: '✦',
+  navigation: { next: true, previous: true },
   footer: {
     text: 'AmirNFT Theme Doc',
   },
+  i18n: [
+    { locale: 'en', text: 'English' },
+    { locale: 'bn', text: 'বাংলা' },
+  ],
 };
